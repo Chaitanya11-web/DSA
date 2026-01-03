c82de1d010bc8a579f7666bb2dc15589e25730b3
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-01-03 13:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (13.23%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-     int n1=0;
-     int h1=0;
-     int pos=0;
-     int i=0;
-     while(i<haystack.length()){
-        if(haystack[i]==needle[n1]){
-            i++;
-            n1++;
-            if(n1==needle.length()){
-                return i-n1;
+        int n = needle.length();
+        if (n == 0) return 0;
+
+        // ---------- LPS ARRAY ----------
+        vector<int> arr(n, 0);
+        int pre = 0, suff = 1;
+
+        while (suff < n) {
+            if (needle[pre] == needle[suff]) {
+                arr[suff] = pre + 1;
+                pre++;
+                suff++;
+            } else {
+                if (pre != 0) {
+                    pre = arr[pre - 1];
+                } else {
+                    suff++;
+                }
             }
         }
-        else{
-pos++;
-i=pos;
-n1=0;
+
+        // ---------- MATCHING ----------
+        int i = 0, j = 0;
+        int m = haystack.length();
+
+        while (i < m) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+            }
+
+            if (j == n) {
+                return i - j;   // match found
+            }
+
+            else if (i < m && haystack[i] != needle[j]) {
+                if (j != 0) {
+                    j = arr[j - 1];   // ✅ CORRECT FALLBACK
+                } else {
+                    i++;
+                }
+            }
         }
-     }
-     return -1;   
+
+        return -1;
     }
 };
