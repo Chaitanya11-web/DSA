1a35e87acb11b4ee01013718f3c851a9656b3e22
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-01-21 15:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.27%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>&image,int i,int j,int color,vector<vector<int>>&visited,int m,int n,int original){
-if(i<0 || j<0 || i>=m || j>=n || visited[i][j]==1 || image[i][j]==color){
+ void dfs(vector<vector<int>>&image,int i,int j,int m,int n,int color,vector<vector<int>>& visited,int original){
+if( i<0 || j<0 || i>=m || j>=n ||visited[i][j]==1 || image[i][j]==color || image[i][j]!=original){
     return;
 }
 else if(image[i][j]==original){
-   image[i][j]=color;
-   visited[i][j]=1;
-  dfs(image,i-1,j,color,visited,m,n,original); 
-   dfs(image,i,j+1,color,visited,m,n,original);
-    dfs(image,i+1,j,color,visited,m,n,original);
-     dfs(image,i,j-1,color,visited,m,n,original);
+    image[i][j]=color;
+    visited[i][j]=1;
 }
-}
+
+
+     dfs(image,i-1,j,m,n,color,visited,original);
+      dfs(image,i+1,j,m,n,color,visited,original);
+    dfs(image,i,j-1,m,n,color,visited,original);
+    dfs(image,i,j+1,m,n,color,visited,original);
+
+ }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size();
-        int n=image[0].size();
-        vector<vector<int>>visited(m,vector<int>(n,0));
-        int original=image[sr][sc];
-        if(original!=color){
-            dfs(image,sr,sc,color,visited,m,n,original);
-        }
-        return image;
+      int original=image[sr][sc];
+      int m=image.size();
+      int n=image[0].size();
+      vector<vector<int>>visited(m,vector<int>(n,0));
+      if(original!=color){
+      dfs(image,sr,sc,m,n,color,visited,original);}
+return image;
     }
 };
