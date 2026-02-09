951a59217367ee2747f4c5884ed0e76f16651d92
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-02-09 11:34:23 +0530

    Time: 1051 ms (37.08%) | Memory: 7.6 MB (87.28%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int n=num;
+        int sum=0;
+        int i=1;
+        while(i<=num/2){
+            if(num%i==0){
+sum+=i;
+
+
+            }
+           i++; 
+        }
+        return (sum==n);
+    }
+};
