22ad4cb5aa74657f58efcb6efbfeaaae8d67a915
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-02-23 18:58:19 +0530

    Time: 59 ms (17.33%) | Memory: 54.9 MB (8.39%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         vector<int>t(n,0);
         t[0]=1;
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=i-1;j>=0;j--){
                 if(j+nums[j]>=i && t[j]==true){
                     t[i]=true;
