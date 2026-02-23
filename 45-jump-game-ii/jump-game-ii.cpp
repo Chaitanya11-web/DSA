837ff2df207561372e877fd333d030ad72210e97
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-02-23 19:12:37 +0530

    Time: 494 ms (5.02%) | Memory: 21.5 MB (21.33%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,18 +3,18 @@ public:
     int jump(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n, INT_MAX);
+        vector<int> t(n, INT_MAX);
         
-        dp[0] = 0;
+        t[0] = 0;
         
         for(int i = 1; i < n; i++){
             for(int j = i-1; j >=0; j--){
                 if(j + nums[j] >= i){
-                    dp[i] = min(dp[i], dp[j] + 1);
+                    t[i] = min(t[i], t[j] + 1);
                 }
             }
         }
         
-        return dp[n-1];
+        return t[n-1];
     }
 };
