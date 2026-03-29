1873f3594d9d47230e706b212229c35a2a89eaa5
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-03-29 21:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (8.62%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n;      // 🟢 FIX: promote to long long
+        long long N = n;      
         double ans = 1;
 
         if (N < 0) {
             x = 1 / x;
-            N = -N;           // 🟢 SAFE now
+            N = -N;           
         }
 
         while (N > 0) {
-            if (N % 2 == 1) {
-                ans = ans * x;
-                N = N - 1;
-            } else {
-                N = N / 2;
-                x = x * x;
+            if(N%2==1){
+                //last me x=1 hojaayega ya no. odd hoga to
+ ans=ans*x;
+N=N-1;
             }
+            else{
+                //ye yaad rakhni h trick like x=2,n=8
+                N=N/2;
+                x=x*x;
+            }
+          
         }
 
         return ans;
