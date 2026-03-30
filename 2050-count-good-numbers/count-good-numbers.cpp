85f6d056c7f3b8686f97e478071656436ecee050
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-03-30 15:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.56%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+long long pow(long long x,long long b){
+    int M= 1e9+7;
+long long res=1;
+//error
+while(b>0){
+if(b%2==1){
+    res=(res*x)%M;
+    //error
+    b--;
+    }
+    else{
+        x=(x*x)%M;
+        b=b/2;
+    }
+}
+return int(res);
+}
+    int countGoodNumbers(long long n) {
+       // ye hamesha modeule karte hue int me ho
+         int M= 1e9+7;
+         //n/2 even place par 4 ko multiply karenge
+         
+         //n+1/2 odd place par 5 ko multiply karenge
+
+      return (pow(4,n/2)*pow(5,((n+1)/2)))%M;  
+    }
+};
