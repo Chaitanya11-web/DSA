fe9d977ba49c82e484626a659ffcbdadc36649cb
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-03-30 21:15:45 +0530

    Time: 1272 ms (5.01%) | Memory: 519.5 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-vector<string>generate(int n,int left,int right,string& temp,vector<string>&ans){
+    vector<string>generate(int n,int left,int right,string temp,vector<string>&ans){
 if(left+right==2*n){
     ans.push_back(temp);
     return ans;
 }
 if(left<n){
-    temp.push_back('(');
-    generate(n,left+1,right,temp,ans);
-    temp.pop_back();
+temp.push_back('(');
+generate(n,left+1,right,temp,ans);
+temp.pop_back();
 }
 if(right<left){
-    temp.push_back(')');
-    generate(n,left,right+1,temp,ans);
-    temp.pop_back();
-}
-return ans;
+temp.push_back(')');
+generate(n,left,right+1,temp,ans);
+temp.pop_back();
 }
 
+ return ans;   }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
         string temp;
-     return generate(n,0,0,temp,ans);  
+        vector<string>ans;
+       return generate(n,0,0,temp,ans);
     }
 };
