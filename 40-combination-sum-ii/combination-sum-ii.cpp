42775f224898f60049fe9f152d9a59572fecfb37
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-03-31 10:36:03 +0530

    Time: 23 ms (6.20%) | Memory: 21.6 MB (5.11%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,56 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> combination(vector<int>& candidates, int n, int i,
-                                    int target, vector<int>& res,
-                                    vector<vector<int>>& ans,
-                                    set<vector<int>>& s) 
-    {
-        // If target reached
-        if(target == 0) {
-            // sort for dedup
-            //vector<int> temp = res;
-            sort(res.begin(), res.end());
-
-           // if(s.find(temp) == s.end()) {
-                ans.push_back(res);
-               // s.insert(temp);
-           // }
-            return ans;
-        }
-
-        if(target < 0 || i >= n) 
-            return ans;
-
-        for(int j = i; j < n; j++) {
-
-            // ✔ Skip duplicates at same recursion level
-            if(j > i && candidates[j] == candidates[j-1])
-                continue;
-
-            // ✔ If number is bigger than remaining target, break
-            if(candidates[j] > target)
-                break;
-
-            // ✔ Choose this number
-            res.push_back(candidates[j]);
-
-            // ✔ Move to next index (never reuse same index)
-            combination(candidates, n, j+1, target - candidates[j], res, ans, s);
-
-            // ✔ Backtrack
-            res.pop_back();
-        }
-
+vector<vector<int>>combine(vector<int>&candidates,int i,int n,int target,vector<int>&res,vector<vector<int>>&ans){
+    if(target==0){
+        sort(res.begin(),res.end());
+        ans.push_back(res);
         return ans;
     }
-
+    if(i==n || target<0){
+        return ans;
+    }
+    for(int j=i;j<n;j++){
+        if(j>i && candidates[j]==candidates[j-1]){
+            continue;
+        }
+        if(candidates[j]>target){
+            break;
+        }
+        res.push_back(candidates[j]);
+        combine(candidates,j+1,n,target-candidates[j],res,ans);
+        res.pop_back();
+    }
+    return ans;
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-
-        sort(candidates.begin(), candidates.end());
-        vector<int> res;
-        set<vector<int>> s;
-
-        return combination(candidates, candidates.size(), 0, target, res, ans, s);
+        int n=candidates.size();
+        sort(candidates.begin(),candidates.end());
+        vector<int>res;
+        vector<vector<int>>ans;
+       return  combine(candidates,0,n,target,res,ans);
     }
 };
