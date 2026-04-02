dfbcf8c5ab253e0ffae6e602c98d868f48eccc67
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-02 17:15:43 +0530

    Time: 841 ms (5.51%) | Memory: 236.5 MB (5.46%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-vector<vector<int>>combine(vector<int>&candidates,int i,int n,int target,vector<int>&res,vector<vector<int>>&ans,set<vector<int>>&st){
-    if(target==0){
-       // sort(res.begin(),res.end());
-        // if(st.find(res)==st.end()){
 
-        //     st.insert(res);
+vector<vector<int>> combine(vector<int>& candidates, int i, int n, int target,
+                           vector<int>& res, vector<vector<int>>& ans){
 
+    // ✅ valid
+    if(target == 0){
         ans.push_back(res);
         return ans;
-        
-        
     }
-    if(i==n ){
-        return ans;
 
+    // ❌ invalid
+    if(i == n || target < 0){
+        return ans;
     }
-    if(target<candidates[i]){
- combine(candidates,i+1,n,target-candidates[i],res,ans,st);
-    }
-    else{
-        // if(j>i && candidates[j]==candidates[j-1]){
-        //     continue;
-        // }
-        // if(candidates[j]>target){
-        //     break;
-        // }
-        res.push_back(candidates[i]);
-        combine(candidates,i+1,n,target-candidates[i],res,ans,st);
-        res.pop_back();
-        int j = i;
-    while(j+1 < n && candidates[j] == candidates[j+1]){
+
+    // ✅ INCLUDE
+    res.push_back(candidates[i]);
+    combine(candidates, i+1, n, target - candidates[i], res, ans);
+    res.pop_back();
+
+    // ✅ EXCLUDE (skip duplicates)
+    int j = i+1;
+    while(j < n && candidates[j] == candidates[j-1]){
         j++;
     }
-         combine(candidates,j+1,n,target,res,ans,st);
-    }
+
+    combine(candidates, j, n, target, res, ans);
+
     return ans;
 }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int n=candidates.size();
-        sort(candidates.begin(),candidates.end());
-        vector<int>res;
-        vector<vector<int>>ans;
-        set<vector<int>>st;
-       return  combine(candidates,0,n,target,res,ans,st);
-    }
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+    sort(candidates.begin(), candidates.end());
+
+    vector<vector<int>> ans;
+    vector<int> res;
+
+    return combine(candidates, 0, candidates.size(), target, res, ans);
+}
 };
