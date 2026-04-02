a0bce2f73d4c44548efd1e049e50037d06aac914
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-02 16:57:41 +0530

    Time: 935 ms (5.51%) | Memory: 267.2 MB (5.46%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-vector<vector<int>>combine(vector<int>&candidates,int i,int n,int target,vector<int>&res,vector<vector<int>>&ans){
+vector<vector<int>>combine(vector<int>&candidates,int i,int n,int target,vector<int>&res,vector<vector<int>>&ans,set<vector<int>>&st){
     if(target==0){
-        sort(res.begin(),res.end());
+       // sort(res.begin(),res.end());
+        // if(st.find(res)==st.end()){
+
+        //     st.insert(res);
+
         ans.push_back(res);
         return ans;
+        
+        
     }
-    if(i==n || target<0){
+    if(i==n ){
         return ans;
+
     }
-    for(int j=i;j<n;j++){
-        if(j>i && candidates[j]==candidates[j-1]){
-            continue;
-        }
-        if(candidates[j]>target){
-            break;
-        }
-        res.push_back(candidates[j]);
-        combine(candidates,j+1,n,target-candidates[j],res,ans);
+    if(target<candidates[i]){
+ combine(candidates,i+1,n,target-candidates[i],res,ans,st);
+    }
+    else{
+        // if(j>i && candidates[j]==candidates[j-1]){
+        //     continue;
+        // }
+        // if(candidates[j]>target){
+        //     break;
+        // }
+        res.push_back(candidates[i]);
+        combine(candidates,i+1,n,target-candidates[i],res,ans,st);
         res.pop_back();
+        int j = i;
+    while(j+1 < n && candidates[j] == candidates[j+1]){
+        j++;
+    }
+         combine(candidates,j+1,n,target,res,ans,st);
     }
     return ans;
 }
@@ -27,6 +42,7 @@ vector<vector<int>>combine(vector<int>&candidates,int i,int n,int target,vector<
         sort(candidates.begin(),candidates.end());
         vector<int>res;
         vector<vector<int>>ans;
-       return  combine(candidates,0,n,target,res,ans);
+        set<vector<int>>st;
+       return  combine(candidates,0,n,target,res,ans,st);
     }
 };
