6f3360fcb717c7812ded299128ae94875d4f5046
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-04 18:58:54 +0530

    Time: 3 ms (41.06%) | Memory: 12.9 MB (35.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-      
-      vector<int>ans;
-      int n1=nums1.size();
-      int n2=nums2.size();
-      unordered_map<int,int>map1;
-     int i=0;
-     while(i<n2){
-        for(int j=i+1;j<n2;j++){
-          
-            if(nums2[j]>nums2[i]){
-                map1[nums2[i]]=nums2[j];
-               
-                break;
-            }
-           
+        int n1=nums1.size();
+        int n2=nums2.size();
+
+       vector<int>ans(n1,-1);
+       stack<int>st;
+       unordered_map<int,int>map1;
+       for(int i=n2-1;i>=0;i--){
+while(!st.empty()&& st.top()<=nums2[i]){
+    st.pop();
+}
+if(!st.empty()){
+    map1[nums2[i]]=st.top();
+}
+st.push(nums2[i]);
+       } 
+      for(int i=0;i<n1;i++){
+        if(map1.find(nums1[i])!=map1.end()){
+            ans[i]=map1[nums1[i]];
         }
-         i++;
-     }
-     for(int j=0;j<n1;j++){
-        if(map1.find(nums1[j])!=map1.end()){
-ans.push_back(map1[nums1[j]]);
-        }
-        else{
-            ans.push_back(-1);
-        }
-     }
-     return ans;
+      }
+      return ans;
     }
 };
