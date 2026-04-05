9901e287f3221ac45b50f190c5100379ea600acb
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-05 23:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.91%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
       stack<char>st;
-      int n=s.length();
+      int n=s.size();
+      
       for(int i=0;i<n;i++){
-        if(s[i]=='('|| s[i]=='['|| s[i]=='{'){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
             st.push(s[i]);
         }
         else{
             if(st.empty())return false;
-            char el=s[i];
-            if(el==')' && st.top()!='(')return false;
- else if(el==']' && st.top()!='[')return false;
-  else if(el=='}' && st.top()!='{')return false;
-  st.pop();
+            if(s[i]==')' && st.top()!='(')return false;
+             if(s[i]==']' && st.top()!='[')return false;
+              if(s[i]=='}' && st.top()!='{')return false;
+              st.pop();
         }
       }
-      return st.empty();  
+      return st.empty();
     }
 };
