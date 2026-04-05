6b690fa59368dc98284c58e3b5b517f906c9228f
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-05 19:01:11 +0530

    Time: 104 ms (28.37%) | Memory: 182.3 MB (27.57%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,42 +1,45 @@
 class LRUCache {
 public:
 list<int>dll;
-map<int,pair<list<int>::iterator,int>>map1;
-int n;
+unordered_map<int,pair<list<int>::iterator,int>>map1;
+int size;
     LRUCache(int capacity) {
-      n=capacity;  
+     size=capacity;   
     }
+    
     void makeRecentlyUsed(int key){
 dll.erase(map1[key].first);
 dll.push_front(key);
 map1[key].first=dll.begin();
     }
     int get(int key) {
-       if(map1.find(key)==map1.end()){
+      if(map1.find(key)==map1.end()){
         return -1;
-       }
-       else{
-        makeRecentlyUsed(key);
-        return map1[key].second;
-       } 
+      }
+int val=map1[key].second;
+makeRecentlyUsed(key);
+return val;
     }
     
     void put(int key, int value) {
-        if(map1.find(key)!=map1.end()){
-            map1[key].second=value;
-            makeRecentlyUsed(key); 
-        }
-        else{
-            dll.push_front(key);
-            map1[key]={dll.begin(),value};
-            n--;
-        }
-        if(n<0){
-            int key_to_Be_Del=dll.back();
-            map1.erase(key_to_Be_Del);
-            dll.pop_back();
-            n++;
-        }
+      if(map1.find(key)!=map1.end()){
+        map1[key].second=value;
+        makeRecentlyUsed(key);
+      }
+      else{
+        dll.push_front(key);
+        map1[key].first=dll.begin();
+        map1[key].second=value;
+        size--;
+      }
+      if(size<0){
+        int el=dll.back();
+        //remember hum log kabhi bhi dll.erase(XXiterator)nhi bhejte hmesha address dete h
+        dll.pop_back();
+        map1.erase(el);
+        size++;
+      }
+
     }
 };
 
