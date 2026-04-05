0be2c031eb8c0aba5311bc890ded6af29be01f9d
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-05 11:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (10.03%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,64 +1,77 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n=nums.size();
-    // long long mod = 1000000007;
-     stack<pair<int,int>>st1;
-     stack<pair<int,int>>st2;
-      stack<pair<int,int>>st3;
-       stack<pair<int,int>>st4;
-     vector<int>nsr(n,n);
-     vector<int>nsl(n,-1);
-    vector<int>ngr(n,n);
-    vector<int>ngl(n,-1);
-     long long ans1=0;
-     long long ans2=0;
-       for(int i=0;i<n;i++){
-while(!st1.empty()&& nums[i]<st1.top().first ){
-    st1.pop();
-}
-if(!st1.empty()){
-    nsl[i]=st1.top().second;
-}
+long long subarrayMax(vector<int>& nums){
+int n=nums.size();
+long long left=0;
+long long right=0;
+long long ans=0;
+stack<pair<int,int>>st1;
+stack<pair<int,int>>st2;
+vector<int>ngl(n,-1);
+vector<int>ngr(n,n);
+for(int i=0;i<n;i++){
+    while(!st1.empty() && st1.top().first<=nums[i]){
+        st1.pop();
+    }
+    if(!st1.empty()){
+        ngl[i]=st1.top().second;
+    }
 st1.push({nums[i],i});
-     } 
-    for(int i=n-1;i>=0;i--){
-while(!st2.empty()&& nums[i]<=st2.top().first ){
-    st2.pop();
 }
-if(!st2.empty()){
-    nsr[i]=st2.top().second;
-}
+for(int i=n-1;i>=0;i--){
+    while(!st2.empty() && st2.top().first<nums[i]){
+        st2.pop();
+    }
+    if(!st2.empty()){
+        ngr[i]=st2.top().second;
+    }
 st2.push({nums[i],i});
-     }  
-for(int i=0;i<n;i++){
-    long long left=i-nsl[i];
-    long long right=nsr[i]-i;
-    ans1+=(left*right)*nums[i];
 }
 for(int i=0;i<n;i++){
-while(!st3.empty()&& nums[i]>st3.top().first ){
-    st3.pop();
+    left=i-ngl[i];
+    right=ngr[i]-i;
+ans+=left*right*nums[i];
 }
-if(!st3.empty()){
-    ngl[i]=st3.top().second;
+return ans;
 }
-st3.push({nums[i],i});
-     } 
-     for(int i=n-1;i>=0;i--){
-while(!st4.empty()&& nums[i]>=st4.top().first ){
-    st4.pop();
+long long subarrayMin(vector<int>& nums){
+int n=nums.size();
+long long left=0;
+long long right=0;
+long long ans=0;
+stack<pair<int,int>>st1;
+stack<pair<int,int>>st2;
+vector<int>nsl(n,-1);
+vector<int>nsr(n,n);
+for(int i=0;i<n;i++){
+    while(!st1.empty() && st1.top().first>=nums[i]){
+        st1.pop();
+    }
+    if(!st1.empty()){
+        nsl[i]=st1.top().second;
+    }
+st1.push({nums[i],i});
 }
-if(!st4.empty()){
-    ngr[i]=st4.top().second;
+for(int i=n-1;i>=0;i--){
+    while(!st2.empty() && st2.top().first>nums[i]){
+        st2.pop();
+    }
+    if(!st2.empty()){
+        nsr[i]=st2.top().second;
+    }
+st2.push({nums[i],i});
 }
-st4.push({nums[i],i});
-     } 
-     for(int i=0;i<n;i++){
-    long long left=i-ngl[i];
-    long long right=ngr[i]-i;
-    ans2+=(left*right)*nums[i];
+for(int i=0;i<n;i++){
+    left=i-nsl[i];
+    right=nsr[i]-i;
+ans+=left*right*nums[i];
 }
- return ans2-ans1;
+return ans;
+}
+
+
+    long long subArrayRanges(vector<int>& nums) {
+        
+        return subarrayMax(nums)-subarrayMin(nums);
     }
 };
