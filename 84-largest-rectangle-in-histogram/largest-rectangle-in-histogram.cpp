bc0f1f9a88865d080c1fd6c7b4f6a0694269ba67
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-06 01:34:42 +0530

    Time: 49 ms (9.05%) | Memory: 86.5 MB (17.94%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,39 +2,35 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        int maxi=INT_MIN;
-      
-        stack<pair<int,int>>st1;
-        stack<pair<int,int>>st2;
         vector<int>nsr(n,n);
         vector<int>nsl(n,-1);
-        int curr=0;
+        int ans=0;
+        int maxi=INT_MIN;
+        stack<pair<int,int>>st1;
+        stack<pair<int,int>>st2;
         for(int i=n-1;i>=0;i--){
-while(!st1.empty() && heights[i]<=st1.top().first){
-    
+while(!st1.empty() && st1.top().first>=heights[i]){
     st1.pop();
 }
 if(!st1.empty()){
     nsr[i]=st1.top().second;
 }
-st1.push({heights[i],i});
+            st1.push({heights[i],i});
         }
         for(int i=0;i<n;i++){
-while(!st2.empty() && heights[i]<=st2.top().first){
-  
+while(!st2.empty() && st2.top().first>=heights[i]){
     st2.pop();
 }
 if(!st2.empty()){
     nsl[i]=st2.top().second;
 }
-st2.push({heights[i],i});
+            st2.push({heights[i],i});
         }
-
         for(int i=0;i<n;i++){
-curr=(nsr[i]-nsl[i]-1)*heights[i];
-if(curr>maxi){
-    maxi=curr;
-}
+            ans=(nsr[i]-nsl[i]-1)*heights[i];
+            if(ans>maxi){
+                maxi=ans;
+            }
         }
         return maxi;
     }
