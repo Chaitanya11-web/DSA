822ef177742d101028f1446101f7157bc3732a7e
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-06 17:04:37 +0530

    Time: 24 ms (14.04%), Space: 48.3 MB (14.09%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-      int n=arr.size();
-      int mod=1000000007;
-      stack<pair<int,int>>st1;
-      stack<pair<int,int>>st2;  
-      vector<int>nsl(n,-1);
-      vector<int>nsr(n,n);
-      long long left=0;
-      long long right=0;
-      long long ans=0;
-      for(int i=0;i<n;i++){
+        int mod=1000000007;
+       int n=arr.size();
+       vector<int>nsl(n,-1);
+       vector<int>nsr(n,n);
+       stack<pair<int,int>>st1;
+       stack<pair<int,int>>st2;
+       long long ans=0;
+       for(int i=n-1;i>=0;i--){
         while(!st1.empty() && st1.top().first>=arr[i]){
-  st1.pop();
+            st1.pop();
         }
-      
-      
-      if(!st1.empty()){
-        nsl[i]=st1.top().second;
-      }
-st1.push({arr[i],i});
-    }
-     for(int i=n-1;i>=0;i--){
+        if(!st1.empty()){
+            nsr[i]=st1.top().second;
+        }
+        st1.push({arr[i],i});
+       } 
+       for(int i=0;i<n;i++){
         while(!st2.empty() && st2.top().first>arr[i]){
-  st2.pop();
+            st2.pop();
         }
-      
-      
-      if(!st2.empty()){
-        nsr[i]=st2.top().second;
-      }
-st2.push({arr[i],i});
+        if(!st2.empty()){
+            nsl[i]=st2.top().second;
+        }
+        st2.push({arr[i],i});
+       } 
+       for(int i=0;i<n;i++){
+        long long left=(i-nsl[i]);
+        long long right=(nsr[i]-i);
+ans+=left*right*arr[i];
+       }
+       return ans%mod;
     }
-    for(int i=0;i<n;i++){
-left=i-nsl[i];
-right=nsr[i]-i;
-ans+=arr[i]*left*right;
-    }
-    //remeber ans can be very long
-    return ans%mod;
-    }  
 };
