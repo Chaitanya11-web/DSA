dd093b143d564753098343914e455bb7d959489d
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-06 19:04:17 +0530

    Time: 4 ms (44.89%), Space: 11.6 MB (39.57%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    string removeKdigits(string nums, int k) {
-        int n=nums.size();
-        //imp
+    string removeKdigits(string num, int k) {
         stack<char>st;
+        int n=num.length();
+       
         for(int i=0;i<n;i++){
-            //K>0 with applied condition of monotonic stack
-            while(!st.empty() && st.top()>nums[i] && k>0){
+while(!st.empty() && st.top()>num[i] && k!=0)
+            {
                 st.pop();
-                k--;
+                    k--;
             }
-            st.push(nums[i]);
+           
+            st.push(num[i]);
         }
-        while(!st.empty() && k>0){
-            st.pop();
-            k--;
+        while(k!=0){
+st.pop();
+k--;
         }
         int n1=st.size();
-        string ans(n1,' ');
-        for(int i=n1-1;i>=0;i--){
-            ans[i]=st.top();
-            st.pop();
-        }
-       int j=0;
-       while(j<n1 && ans[j]=='0'){
-        j++;
-       }
+         string ans(n1,' ');
+for(int i=n1-1;i>=0;i--){
+    ans[i]=st.top();
+    st.pop();
+}
+int j=0;
+while(ans[j]=='0'){
+    j++;
+}
 if(ans.substr(j)==""){
     return "0";
 }
-return ans.substr(j);
-    }
+  return ans.substr(j);  }
 };
