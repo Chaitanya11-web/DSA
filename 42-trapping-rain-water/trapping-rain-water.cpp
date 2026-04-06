99dfee86cd4139762cbe3e128325aaa8cd4fa42b
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-06 16:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (16.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-void leftMax(vector<int>&height,int n,vector<int>& left){
-for(int i=1;i<n;i++){
-    // left[i-1] aayega jo val mil gyi usse compare karo
-    left[i]=max(height[i],left[i-1]);
-}
-
-}
-void rightMax(vector<int>&height,int n,vector<int>& right){
+void findRightMax(vector<int>&rightMax,int n,vector<int>&height){
 for(int i=n-2;i>=0;i--){
-    // right[i+1] aayega jo val mil gyi usse compare karo
-    right[i]=max(height[i], right[i+1]);
+    rightMax[i]=max(height[i],rightMax[i+1]);
+}
+}
+void findLeftMax(vector<int>&leftMax,int n,vector<int>&height){
+for(int i=1;i<n;i++){
+    leftMax[i]=max(height[i],leftMax[i-1]);
 }
 }
     int trap(vector<int>& height) {
-        int sum=0;
-      int n=height.size();
-      vector<int>left(n,height[0]);
-      vector<int>right(n,height[n-1]);
-leftMax(height,n,left);
-rightMax(height,n,right);
-for(int i=0;i<n;i++){
-    sum+=min(left[i],right[i])-height[i];
-}
-return sum;
+       int n=height.size();
+       int ans=0;
+       vector<int>leftMax(n,height[0]) ;
+      vector<int> rightMax(n,height[n-1]);
+      findRightMax(rightMax,n,height);
+      findLeftMax(leftMax,n,height);
+       for(int  i=1;i<n-1;i++){
+        ans+=min(leftMax[i],rightMax[i])-height[i];
+       }
+       return ans;
     }
 };
