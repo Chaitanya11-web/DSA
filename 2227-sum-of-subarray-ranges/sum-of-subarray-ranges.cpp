28b581f5e13ee9bd9d10ea4852442921399c6605
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-06 18:02:29 +0530

    Time: 5 ms (51.26%) | Memory: 15.8 MB (52.37%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,77 +1,79 @@
 class Solution {
 public:
-long long subarrayMax(vector<int>& nums){
-int n=nums.size();
-long long left=0;
-long long right=0;
-long long ans=0;
-stack<pair<int,int>>st1;
-stack<pair<int,int>>st2;
-vector<int>ngl(n,-1);
-vector<int>ngr(n,n);
-for(int i=0;i<n;i++){
-    while(!st1.empty() && st1.top().first<=nums[i]){
-        st1.pop();
-    }
-    if(!st1.empty()){
-        ngl[i]=st1.top().second;
-    }
-st1.push({nums[i],i});
-}
-for(int i=n-1;i>=0;i--){
-    while(!st2.empty() && st2.top().first<nums[i]){
-        st2.pop();
-    }
-    if(!st2.empty()){
-        ngr[i]=st2.top().second;
-    }
-st2.push({nums[i],i});
-}
-for(int i=0;i<n;i++){
-    left=i-ngl[i];
-    right=ngr[i]-i;
+long long sumSubarrayMin(vector<int>&nums){
+ int mod=1000000007;
+       int n=nums.size();
+       vector<int>nsl(n,-1);
+       vector<int>nsr(n,n);
+       stack<pair<int,int>>st1;
+       stack<pair<int,int>>st2;
+       long long ans=0;
+       for(int i=n-1;i>=0;i--){
+        while(!st1.empty() && st1.top().first>=nums[i]){
+            st1.pop();
+        }
+        if(!st1.empty()){
+            nsr[i]=st1.top().second;
+        }
+        st1.push({nums[i],i});
+       } 
+       for(int i=0;i<n;i++){
+        //remember dono jagah include nhi karna = equal value ko
+        while(!st2.empty() && st2.top().first>nums[i]){
+            st2.pop();
+        }
+        if(!st2.empty()){
+            nsl[i]=st2.top().second;
+        }
+        st2.push({nums[i],i});
+       } 
+       for(int i=0;i<n;i++){
+        long long left=(i-nsl[i]);
+        long long right=(nsr[i]-i);
 ans+=left*right*nums[i];
-}
-return ans;
-}
-long long subarrayMin(vector<int>& nums){
-int n=nums.size();
-long long left=0;
-long long right=0;
-long long ans=0;
-stack<pair<int,int>>st1;
-stack<pair<int,int>>st2;
-vector<int>nsl(n,-1);
-vector<int>nsr(n,n);
-for(int i=0;i<n;i++){
-    while(!st1.empty() && st1.top().first>=nums[i]){
-        st1.pop();
-    }
-    if(!st1.empty()){
-        nsl[i]=st1.top().second;
-    }
-st1.push({nums[i],i});
-}
-for(int i=n-1;i>=0;i--){
-    while(!st2.empty() && st2.top().first>nums[i]){
-        st2.pop();
-    }
-    if(!st2.empty()){
-        nsr[i]=st2.top().second;
-    }
-st2.push({nums[i],i});
-}
-for(int i=0;i<n;i++){
-    left=i-nsl[i];
-    right=nsr[i]-i;
-ans+=left*right*nums[i];
-}
-return ans;
+       }
+       return ans;
 }
 
+
+long long sumSubarrayMax(vector<int>&nums){
+ int mod=1000000007;
+       int n=nums.size();
+       vector<int>ngl(n,-1);
+       vector<int>ngr(n,n);
+       stack<pair<int,int>>st1;
+       stack<pair<int,int>>st2;
+       long long ans=0;
+       for(int i=n-1;i>=0;i--){
+        while(!st1.empty() && st1.top().first<=nums[i]){
+            st1.pop();
+        }
+        if(!st1.empty()){
+            ngr[i]=st1.top().second;
+        }
+        st1.push({nums[i],i});
+       } 
+       for(int i=0;i<n;i++){
+        //remember dono jagah include nhi karna = equal value ko
+        while(!st2.empty() && st2.top().first<nums[i]){
+            st2.pop();
+        }
+        if(!st2.empty()){
+            ngl[i]=st2.top().second;
+        }
+        st2.push({nums[i],i});
+       } 
+       for(int i=0;i<n;i++){
+        long long left=(i-ngl[i]);
+        long long right=(ngr[i]-i);
+ans+=left*right*nums[i];
+       }
+       return ans;
+}
 
     long long subArrayRanges(vector<int>& nums) {
-        
-        return subarrayMax(nums)-subarrayMin(nums);
+      int n1=nums.size();
+      int mod=1000000007;
+      return (sumSubarrayMax(nums)-sumSubarrayMin(nums));  
     }
 };
