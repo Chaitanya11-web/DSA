658865e4ba7f6620d13d6bbeff8b2ddead31b867
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-07 18:51:45 +0530

    Time: 3 ms (77.95%) | Memory: 11.5 MB (62.76%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-bool isAllZero(unordered_map<char,int>&map){
-           for(auto it:map){
-               int cnt=it.second;
-               if(cnt!=0){
-                   return false;
-               }
-           }
-           return true;
-       }
+bool allZero(unordered_map<int,int>&map1){
+
+for(auto it:map1){
+if(it.second!=0){
+    return false;
+}
+}
+return true;
+}
     vector<int> findAnagrams(string s, string p) {
-         vector<int>ans;
-                unordered_map<char,int>map1;
-        int sum=0;
-        int cntStr=0;
-        int n=s.length();
-        int m=p.length();
-        for(int i=0;i<m;i++){
-            map1[p[i]]++;
+      int m=s.size();
+      int n=p.size();
+      int i=0;
+      int j=0;
+      int cnt=0;
+      vector<int>ans;
+      unordered_map<int,int>map1;
+      for(int i=0;i<n;i++){
+        map1[p[i]]++;
+      }
+      while(j<m){
+        if(map1.find(s[j])!=map1.end()){
+            map1[s[j]]--;
         }
-        
-        int i=0;
-        int j=0;
-      
-        while(j<n){
-            if(map1.find(s[j])!=map1.end()  ){
-               
-                map1[s[j]]--;
-                
-            }
-           if(j-i+1==m){
-               if(isAllZero(map1)){
-                ans.push_back(i);
-                cntStr+=1;   
-               }
-               if(map1.find(s[i])!=map1.end()){
-                   map1[s[i]]+=1;
-                   
-               }
-              i++; 
-           } 
-            
-            
-            j++;
-        }
-        return ans;
+if(j-i+1==n){
+    if(allZero(map1)){
+        cnt++;
+ans.push_back(i);
+}
+
+if(map1.find(s[i])!=map1.end()){
+map1[s[i]]++;
+}
+i++;
+    
+   
+   
+}
+
+j++;
+      }
+      return ans;
     }
 };
