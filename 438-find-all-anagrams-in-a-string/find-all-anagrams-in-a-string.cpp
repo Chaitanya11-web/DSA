06828a28b9cb1ea804b8f2c630a33d7fdf5fdb63
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-10 00:52:40 +0530

    Time: 7 ms (48.46%) | Memory: 11.4 MB (83.86%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,46 +1,39 @@
 class Solution {
 public:
-bool allZero(unordered_map<int,int>&map1){
-
+bool allZero(unordered_map<char,int>&map1){
 for(auto it:map1){
-if(it.second!=0){
-    return false;
-}
+    if(it.second!=0){
+        return false;
+    }
 }
 return true;
 }
     vector<int> findAnagrams(string s, string p) {
-      int m=s.size();
-      int n=p.size();
-      int i=0;
-      int j=0;
-      int cnt=0;
-      vector<int>ans;
-      unordered_map<int,int>map1;
-      for(int i=0;i<n;i++){
-        map1[p[i]]++;
-      }
-      while(j<m){
-        if(map1.find(s[j])!=map1.end()){
-            map1[s[j]]--;
+        int m=s.length();
+        int n=p.length();
+        int i=0;
+        int j=0;
+        vector<int>ans;
+        unordered_map<char,int>map1;
+        for(int i=0;i<n;i++){
+            map1[p[i]]++;
         }
-if(j-i+1==n){
-    if(allZero(map1)){
-        cnt++;
-ans.push_back(i);
+        while(j<m){
+if(map1.find(s[j])!=map1.end()){
+map1[s[j]]--;
 }
-
+if(j-i+1==n){
+if(allZero(map1)){
+    ans.push_back(i);
+}
 if(map1.find(s[i])!=map1.end()){
-map1[s[i]]++;
+    map1[s[i]]++;
 }
 i++;
-    
-   
-   
 }
 
-j++;
-      }
-      return ans;
+            j++;
+        }
+        return ans;
     }
 };
