7490a7cd79bf314e78f1f33c79e0092fcb8d3028
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-13 18:52:44 +0530

    Time: 18 ms (12.48%) | Memory: 11.1 MB (18.17%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-       int n=s.length();
-       unordered_map<int,int>map1;
-       int maxFreq=0;
-       int cntStr=0;
-       int i=0;
-       int j=0;
-       while(j<n){
-map1[s[j]]++;
-if(maxFreq<map1[s[j]]){
-    maxFreq=map1[s[j]];
-}
-if((j-i+1)-maxFreq<=k){
-    if(cntStr<(j-i+1)){
-        cntStr=j-i+1;
-    }
-}
-else{
-    if((j-i+1)-maxFreq>k){
-        map1[s[i]]--;
-        i++;
-    }
-}
-
-        j++;
-       } 
-       return cntStr;
+        int n=s.length();
+        int maxFreq=0;
+        unordered_map<char,int>map1;
+        int maxLen=INT_MIN;
+        int i=0;
+        int j=0;
+        while(j<n){
+            map1[s[j]]++;
+            if(maxFreq<map1[s[j]]){
+                maxFreq=map1[s[j]];
+                
+            }
+            if((j-i+1)-maxFreq<=k){
+                maxLen=max(maxLen,j-i+1);
+            }
+              if ((j-i+1)-maxFreq>k){
+                map1[s[i]]--;
+                if(map1[s[i]]==0){
+                    map1.erase(s[i]);
+                }
+i++;
+            }
+            j++;
+        }
+       return maxLen;
     }
 };
