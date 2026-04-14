feb65949586ae62deb86a5e63776ebea9d5a68c9
Chaitanya11-web <chaitanyatyagi242@gmail.com>
2026-04-14 11:31:52 +0530

    Time: 80 ms (66.92%), Space: 56.1 MB (57.47%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-int subarray(vector<int>&nums,int k){
-int n=nums.size();
-int cnt=0;
+int calc(vector<int>&nums,int k){
+    if(k<0)return 0;
+    int n=nums.size();
+int cnt1=0;
 unordered_map<int,int>map1;
 int i=0;
 int j=0;
 while(j<n){
-map1[nums[j]]++;
-if(map1.size()<=k){
-cnt+=j-i+1;
-}
+    map1[nums[j]]++;
+// if(map1.size()<=k){
+//     cnt1+=(j-i+1);
 
-while(map1.size()>k && i<n){
-map1[nums[i]]--;
+// }
+while(map1.size()>k ){
+    map1[nums[i]]--;
 if(map1[nums[i]]==0){
     map1.erase(nums[i]);
 }
-i++;
-if(map1.size()<=k){
-    cnt+=j-i+1;
-}
 
+i++;
 }
+//yaad rakhna jab map1.size()>k check kar liya to ek baar uske baad bhi check karo kyuki size map ka reduce ho gya hoga
+
+    cnt1+=(j-i+1);
+
 
     j++;
 }
-
-return cnt;
+return cnt1;
 }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-   int n=nums.size();
-   return subarray(nums,k)-subarray(nums,k-1);  
+        int n=nums.size();
+        return calc(nums,k)-calc(nums,k-1);
     }
 };
